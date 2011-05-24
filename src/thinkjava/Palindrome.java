bc433c741dba9dc31b5baedfb6c0558de36a157b
Karlo Smid <karlo.smid@gmail.com>
2011-05-24 20:54:36 +0200

    finished up to exercise 7.8

--- src/thinkjava/Palindrome.java
+++ src/thinkjava/Palindrome.java
@@ -10,8 +10,48 @@ package thinkjava;
  * @author karlo
  */
 public class Palindrome {
+    public static boolean isDupledrome(String s){
+        if (s.length()==2)
+            if (s.charAt(0) == s.charAt(1) ){
+                return true;
+            }
+            else{
+                return false;
+            }
+        else
+            if (s.charAt(0) == s.charAt(1) ){
+                return isDupledrome(s.substring(2, s.length()));
+            }
+            else{
+                return false;
+            }
+    }
+
+    public static boolean isAbecedarian(String s){
+        if (s.length()==1)
+            return true;
+        else
+            if (s.charAt(0) <= s.charAt(1) ){
+                return isAbecedarian(s.substring(1, s.length()));
+            }
+            else{
+                return false;
+            }        
+    }
+    public static boolean isPalindromeIter(String s){
+        if (s.length()>=0 && s.length()<=1){
+            return true;
+        }else{
+            for(int i = 0;i<s.length()/2;i++){
+                if(s.charAt(i) != s.charAt(s.length()-1-i)){
+                    return false;
+                }
+            }
+        return true;
+        }
+    }
     public static boolean isPalindrome(String s){
-        System.out.println(s);
+//        System.out.println(s);
         if (s.length()==1){
             return true;            
             
@@ -47,13 +87,25 @@ public class Palindrome {
         return s.charAt(s.length()-1);
     }
     public static void main(String[] args){
-        String s = "Karlo";
+//        String s = "Karlo";
 //        System.out.println(first(s));
 //        System.out.println(last(s));
 //        System.out.println(middle(""));
-        System.out.println(isPalindrome("otto"));
-        System.out.println(isPalindrome("acaramanamaraca"));
-        System.out.println(isPalindrome("anavolimilovana"));
+//        System.out.println(isPalindrome("otto"));
+//        System.out.println(isPalindrome("acaramanamaraca"));
+//        System.out.println(isPalindrome("anavolimilovana"));
+//        System.out.println(isPalindrome("karlo"));
+//
+//        System.out.println(isPalindromeIter("otto"));
+//        System.out.println(isPalindromeIter("acaramanamaraca"));
+//        System.out.println(isPalindromeIter("anavolimilovana"));
+//        System.out.println(isPalindromeIter("karlo"));
+//        System.out.println(isAbecedarian("abdest"));
+//        System.out.println(isAbecedarian("karlo"));
+//        System.out.println(isAbecedarian("biopsy"));
+
+        System.out.println(isDupledrome("karlo"));
+        System.out.println(isDupledrome("kkaarrlloo"));
     }
 
 }
